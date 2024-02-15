9f18413e74675629bea8618623b6e95650050893
jacobmc352 <jacobmc352@gmail.com>
2024-02-14 19:55:48 -0600

    test/helloworld.js

---
+++ test/helloworld.test.js
@@ -0,0 +1,10 @@
+import sayHello from '../utils/helloworld.mjs';
+import assert from 'assert';
+
+it("Tests Hello world", ()=>{
+
+    const hello = sayHello();
+
+    assert.equal(hello,"hello");
+
+});
